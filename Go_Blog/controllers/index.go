dca55d3d06d5dd7dcf7defa8964aec339ca7c33d
micor6588 <micor5688@163.com>
2020-04-07 09:32:58 +0800

    添加About页面显示

--- Go_Blog/controllers/index.go
+++ Go_Blog/controllers/index.go
@@ -8,6 +8,11 @@ type IndexController struct {
 	beego.Controller
 }
 
+// @router /about [get]
+func (c *IndexController) IndexAbout() {
+	c.TplName = "about.html"
+}
+
 // @router / [get]
 func (c *IndexController) Index() {
 	c.TplName = "index.html"
--- Go_Blog/routers/commentsRouter_controllers.go
+++ Go_Blog/routers/commentsRouter_controllers.go
@@ -16,4 +16,13 @@ func init() {
             Filters: nil,
             Params: nil})
 
+    beego.GlobalControllerRouter["BeeGO_Blog/Go_Blog/controllers:IndexController"] = append(beego.GlobalControllerRouter["BeeGO_Blog/Go_Blog/controllers:IndexController"],
+        beego.ControllerComments{
+            Method: "IndexAbout",
+            Router: `/about`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
 }
